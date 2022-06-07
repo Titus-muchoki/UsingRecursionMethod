5ec55ff7fa5ad114e78b6cac65a1ef3255fbb0ee
kajela <titoyut56@gmail.com>
2022-06-07 01:27:21 -0400

    add the initial files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,17 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int result = Sum(20);
+            System.out.println(result);
+        }
+
+    public static int Sum(int k) {
+        if ( k > 0){
+            return k + Sum(k - 1 );
+        }else {
+            return 0;
+        }
     }
-}
+
+    }
